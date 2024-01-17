f6bf6e33105250147ec944a1c02171b0c8007e3a
Valter Backström <dylandante@gmail.com>
2024-01-17 11:01:23 +0200

    Lisättiin index.js tiedosto ja muokattiin index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+//Palvelut sivun näyttäminen
+function palvelut() {
+    let palvelut = `<h2>Palvelumme</h2>
+   
+   
+          <p>Eläinlääkäri</p>
+          <p>Päiväjoito</p>
+          <p>Yöhoito</p>
+   
+    
+    `
+    document.getElementById("sisältö").innerHTML = palvelut
+
+}
